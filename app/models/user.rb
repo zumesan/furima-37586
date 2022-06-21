d3733bc5df04c916df1c730109e88a240b9f6d9b
zumesan <zumehashi3240518@icloud.com>
2022-06-22 01:22:02 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ApplicationRecord
   has_many :products
   has_many :purchase_records
 
+  VALID_PASSWORD_REGEX = /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{7,}/
+  FULL_WIDTH = "is invalid. Input full-width characters"
+  FULL_WIDTH_KANA = "is invalid. Input full-width katakana characters"
 
+  validates :nickname, presence:true
+  validates :password,length: { minimum:6, allow_blank: true },format:{with: VALID_PASSWORD_REGEX, message:"is invalid. Include both letters and numbers" }
+  validates :first_name, presence:true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:FULL_WIDTH}
+  validates :last_name, presence:true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:FULL_WIDTH}
+  validates :first_name_kana, presence:true, format:{with: /\A[ァ-ヴー]+\z/u,message:FULL_WIDTH_KANA}
+  validates :last_name_kana, presence:true, format:{with: /\A[ァ-ヴー]+\z/u,message:FULL_WIDTH_KANA}
   validates :birth_date, presence:true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+end
