3ee82dd9e9767896db94fcea6a31c51c9dbeb768
zumesan <zumehashi3240518@icloud.com>
2022-06-21 14:23:04 +0900

    新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller? #deviseに関するコントローラーの処理の場合にメソッドを実行する
 
   private
 
@@ -7,5 +8,12 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  #sign_upの処理の許可
+  def configure_permitted_parameters #メソッド名は慣習
+    # deviseのUserモデルにパラメーターを許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+    # devise_parameter_sanitizer.permit(:deviseの処理名, keys: [:許可するキー])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'items#index'
-  resources :items do
+  resources :items, only: :new do
   end
 end
