3e0eda186bb08844a4d95ea6c9c900dd0b7540b0
zumesan <zumehashi3240518@icloud.com>
2022-07-17 18:15:58 +0900

    ordersコントローラーに、自信が出品した商品購入ページに遷移すると、トップページに遷移するメソッドを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,24 @@
 class OrdersController < ApplicationController
 
+  before_action :set_product, only: :index
+  before_action :contributor_confirmation, only: [:index, :create]
+
   def index
-    @product = Product.find(params[:item_id])
+
   end
 
   def create
+  end
+
+
+  private
+
+  def set_product
+    @product = Product.find(params[:item_id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path if current_user == @product.user
   end
 
 end
