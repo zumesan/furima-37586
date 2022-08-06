b7d6b4d8f5be3d0bf86fa7b133d173fc07fc6824
zumesan <zumehashi3240518@icloud.com>
2022-08-07 04:32:40 +0900

    プレビュー機能の実装

--- db/migrate/20220801190907_create_comments.rb
+++ db/migrate/20220801190907_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.interger :user_id
-      t.interger :product_id
+      t.integer :user_id
+      t.integer :product_id
 
       t.timestamps
     end
