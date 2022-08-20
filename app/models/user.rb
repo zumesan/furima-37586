a79aaf04d60c7d80f22809d1f5ef2c8922e12c95
zumesan <zumehashi3240518@icloud.com>
2022-08-20 19:58:19 +0900

    日本語化2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,11 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i #半角英数のバリデーションの値を変数に格納
   VALID_PASSWORD_REGEX.freeze #freezeで変数の内容を固定
-  FULL_WIDTH = "is invalid. Input full-width characters"
-  FULL_WIDTH_KANA = "is invalid. Input full-width katakana characters" #全角カナ文字のバリデーションのエラーメッセージを変数に格納
+  FULL_WIDTH = "は全角で入力してください"
+  FULL_WIDTH_KANA = "は全角カタカナで入力してください" #全角カナ文字のバリデーションのエラーメッセージを変数に格納
 
   validates :nickname, presence:true
-  validates :password, format:{with: VALID_PASSWORD_REGEX, message:"is invalid. Include both letters and numbers" } #半角英数のバリデーション
+  validates :password, format:{with: VALID_PASSWORD_REGEX, message:"は半角英数字で入力してください" } #半角英数のバリデーション
   #ここから全角文字のバリデーション
   validates :first_name, presence:true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:FULL_WIDTH}
   validates :last_name, presence:true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message:FULL_WIDTH}
